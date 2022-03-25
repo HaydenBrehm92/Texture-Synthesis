1df6480d2756c7b6ee82a88a6a96e638a41c7fc4
haydenbrehm92@gmail.com <haydenbrehm92@gmail.com>
2022-03-25 17:09:19 -0500

    Added: my attempt at texture synthesis + pictures Removed: synth.cpp synth.h

--- Texture Synthesis/Image.h
+++ Texture Synthesis/Image.h
@@ -2,11 +2,15 @@
 #define _Image_h
 
 #include <vector>
+#include <tuple>
+#include <utility>
+#include <random>
 #include <fstream>
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <boost/filesystem.hpp>
 
 
@@ -18,12 +22,17 @@ public:
 
 	std::vector<boost::filesystem::path> GetFiles();
 	std::vector<cv::Mat> GetImages();
-	
+
 	void ReadImages();
-	void WriteImages();
+	void WriteImages(cv::Mat & image, int currPos, int windowSize);
 	void Synthesis();
 private:
-	cv::Mat GrowImage(cv::Mat & imageSample,cv::Mat & image, cv::Mat windowSize);
+	cv::Mat GrowImage(const cv::Mat & imageSample,cv::Mat & image, int windowSize);
+	cv::Mat GetUnfilledNeighbors(cv::Mat & image);
+	cv::Rect GetNeighborhoodWindow(const cv::Mat image, const int windowSize, const cv::Point pixel);
+	std::vector<std::tuple<float, cv::Point>> FindMatches(const cv::Mat roi, const cv::Mat & imageSample, const int windowSize);
+	cv::Vec3b getColor(const cv::Point location, const cv::Mat& imagesSample);
+	std::tuple<int,int> getXandYoffsets(const cv::Mat image, const int windowSize, const cv::Point pixel);
 	void SetPaths();
 	void CreateDirectory();
 
